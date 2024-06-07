95da4f0657c95520751e4494c4de34d713a09504
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-06-07 16:36:31 -0400

    Se realizo los servicios inciales GET de cars

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
+
+@Controller('cars')
+export class CarsController {
+
+    constructor(
+        private readonly carsService: CarsService
+      ) {}
+
+    @Get()
+    getAllCars(){
+        return this.carsService.findAll()
+    }
+
+    @Get(':id')
+    getCarById( @Param('id' ) id: number ) {
+  
+        console.log({id})
+      return this.carsService.findOneById(id)
+    }
+}
