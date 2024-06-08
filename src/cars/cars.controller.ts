b379a34caf561b173e646ffe174c7ddcf80cccf5
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-06-08 06:03:13 -0400

    Se agregó un pipe para convertir string a numero

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -14,7 +14,7 @@ export class CarsController {
     }
 
     @Get(':id')
-    getCarById( @Param('id' ) id: number ) {
+    getCarById( @Param('id', ParseIntPipe ) id: number ) {
   
         console.log({id})
       return this.carsService.findOneById(id)
